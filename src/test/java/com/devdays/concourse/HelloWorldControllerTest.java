3f23126abbcc9e9e8907cae082a37bf294875de8
Arthur Maltson <arthur@maltson.com>
2016-05-30 10:39:40 -0400

    Create TestNG test
    Remove JUnit from application level test

--- src/test/java/com/devdays/concourse/HelloWorldControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.devdays.concourse;
-
-/**
- * Created by arthur on 16-05-30.
- */
-public class HelloWorldControllerTest {
-
-}
---
+++ src/test/java/com/devdays/concourse/HelloWorldControllerTests.java
@@ -0,0 +1,12 @@
+package com.devdays.concourse;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloWorldControllerTests {
+
+  @Test
+  public void sanityTest() {
+    Assert.assertEquals(1, 1);
+  }
+}
